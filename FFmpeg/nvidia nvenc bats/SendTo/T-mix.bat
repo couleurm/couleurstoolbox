272af96c7fea23d344416ea187744ef344c25452
Couleur <couleur@riseup.net>
2021-05-10 19:12:40 +0200

    nvenc script fixes

--- FFmpeg/nvidia nvenc bats/SendTo/T-mix.bat
+++ FFmpeg/nvidia nvenc bats/SendTo/T-mix.bat
@@ -1,4 +1,5 @@
-ffmpeg -hwaccel cuda -i %1 -vf xbr=2 -c:v hevc_nvenc -rc constqp -qp 20 -preset p7 -c:a copy -r 60 "%~dpn1 (Resampled).mp4"
+ffmpeg -i %1 -vf tmix=frames=8:weights="1" -c:v h264_nvenc -preset slow -r 60 "%~dpn1 (Resampled).mp4
 pause
 
-::test
+::Change value after tmix=frames= to be the number of blur frames
+::Change value after -r to be the FPS you want to render in
--- FFmpeg/nvidia nvenc bats/SendTo/advanced/720pTo4K XBR nvenc.bat
+++
@@ -1,4 +0,0 @@
-ffmpeg -i %1 -vf xbr=3 -c:v h264_nvenc -profile:v high -preset fast -qmin 0 -qmax 1 -cq 51 "%~dpn1 (4K).mp4"
-pause
-
-::if you change the value after XBR it'll change how much times it multiplies the resolution.
