35c1f269967635398d55202aa1ae209cdbaa7361
Couleur <couleur@riseup.net>
2021-05-12 20:44:41 +0200

    ddu/nvlceanstall - markc accel fix - discord links

--- FFmpeg/nvidia nvenc bats/SendTo/T-mix.bat
+++ FFmpeg/nvidia nvenc bats/SendTo/T-mix.bat
@@ -1,4 +1,4 @@
-ffmpeg -i %1 -vf tmix=frames=8:weights="1" -c:v h264_nvenc -preset slow -r 60 "%~dpn1 (Resampled).mp4
+ffmpeg -i %1 -vf tmix=frames=8:weights="1" -c:v h264_nvenc -profile:v high -preset slow -qmin 0 -qmax 1 -cq 51 -r 60 "%~dpn1 (Resampled).mp4
 pause
 
 ::Change value after tmix=frames= to be the number of blur frames
