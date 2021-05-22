b8f327b09cb7bb0d1cc6bfe2a2d1ee4c012bda9b
vladaad <59199127+vladaad@users.noreply.github.com>
2021-05-22 21:34:11 +0200

    Fix colours on NVIDIA scale scripts

--- 7  FFmpeg/Nvidia NVENC bats/SendTo/1080pTo4K nearestneighbor.bat
+++ 7  FFmpeg/Nvidia NVENC bats/SendTo/1080pTo4K nearestneighbor.bat
@@ -1,2 +1,2 @@
-ffmpeg -hwaccel cuda -hwaccel_output_format cuda -threads 8 -i %1 -vf scale_cuda=3840:2160:1 -c:v hevc_nvenc -preset p7 -rc constqp -qp 18 -c:a copy "%~dpn1 (4K).mp4"
+ffmpeg -hwaccel cuda -i %1 -vf scale=3840:2160:flags=neighbor -c:v hevc_nvenc -preset p7 -rc constqp -qp 18 -c:a copy "%~dpn1 (4K).mp4"
 pause
