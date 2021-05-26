091334504d2947c9188a973b1cbba1b8697caec8
Couleur <couleur@riseup.net>
2021-05-26 12:14:29 +0200

    Added automatic batch file elevation in executor

--- 2  Programs/Chocolatey/Executor.bat
+++ 2  Programs/Chocolatey/Executor.bat
@@ -1,1 +1,4 @@
+@echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 powershell iex .\Choco_GUI.ps1
