03bbe2fe66d9deee5a434e65e7561d107c13312a
Couleur <couleur@riseup.net>
2021-05-30 01:46:38 +0200

    updated lc version

--- 6 LunarClient/Download & LunarClient.bat
+++ 6 LunarClient/Download & LunarClient.bat
@@ -1,5 +1,5 @@
 @echo Downloading Lunar Client, please wait
 @echo off
-powershell Invoke-WebRequest "https://launcherupdates.lunarclientcdn.com/Lunar%20Client%20v2.7.2.exe" -OutFile "%homepath%\Downloads\LunarSetup.exe"
+powershell Invoke-WebRequest "https://launcherupdates.lunarclientcdn.com/Lunar%20Client%20v2.7.3.exe" -OutFile "%homepath%\Downloads\LunarSetup.exe"
 echo Download done, execution is imminent
 %homepath%\Downloads\LunarSetup.exe
