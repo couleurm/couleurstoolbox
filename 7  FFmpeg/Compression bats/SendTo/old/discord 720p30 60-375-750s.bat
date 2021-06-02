adcd13ffdffbc2f14a29d3e09da7037f213b0515
vladaad <59199127+vladaad@users.noreply.github.com>
2021-06-02 12:13:15 +0200

    Refactor ffmpeg category

--- 7  FFmpeg/Compression bats/SendTo/old/discord 720p30 60-375-750s.bat
+++
@@ -1,5 +0,0 @@
-ffmpeg -i %1 -c:v libx264 -vf "fps=30,scale='-2':'min(720,ih):flags=lanczos'" -preset veryslow -b:v 955k -g 480 -x264-params qpmin=18 -pass 1 -f null NUL
-ffmpeg -i %1 -c:v libx264 -vf "fps=30,scale='-2':'min(720,ih):flags=lanczos'" -preset veryslow -b:v 955k -g 480 -x264-params qpmin=18 -pass 2 -c:a aac -b:a 128k -movflags +faststart "%~dpn1 (compressed).mp4"
-del ffmpeg2pass-0.log
-del ffmpeg2pass-0.log.mbtree
-pause
