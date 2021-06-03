ba843a777d7a5428812c8b93568e2c53656bffc8
Couleur <couleur@riseup.net>
2021-06-03 20:15:01 +0200

    Opens toolbox in explorer after installation

--- GitHub/install the toolbox.ps1
+++ GitHub/install the toolbox.ps1
@@ -1,12 +1,9 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
 choco install git -y
-
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
-
 cd $env:userprofile\Desktop
-
 powershell git clone https://github.com/couleurm/couleurstoolbox.git
-pause
+Start-Process $env:userprofile\Desktop\couleurstoolbox
+
