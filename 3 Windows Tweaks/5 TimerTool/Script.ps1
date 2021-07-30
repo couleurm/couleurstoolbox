c004debd0b9646303733142666eff412dfd7bc51
Lisa <broxmodz@gmail.com>
2021-07-30 23:14:25 +0200

    Dont use ILSCL. RAM Management is good since Win 1909. Also using TimerTool Systemservice for even less Proccesses and shit running

--- 3 Windows Tweaks/5 TimerTool/Script.ps1
+++ 3 Windows Tweaks/5 TimerTool/Script.ps1
@@ -6,6 +6,3 @@ Invoke-WebRequest -Uri $url -OutFile "service.ps1"
     Write-Host "Installed TimerTool Service. Please restart..."
     Remove-Item -Path "service.ps1"
     Remove-Item -Path "STR_Install.log"
-    
-
-    C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; iwr -useb https://raw.githubusercontent.com/PrincessAkira/Hosting-Repo/main/ServiceInstaller.ps1 | iex
