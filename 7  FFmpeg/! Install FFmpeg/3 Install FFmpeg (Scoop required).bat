af622f8a9268ac93003df2983a5fbfeef52e1267
Lisa <broxmodz@gmail.com>
2021-10-14 17:39:27 +0200

    renamed file

--- 7  FFmpeg/! Install FFmpeg/3 Install FFmpeg (Scoop required).bat
+++ 7  FFmpeg/! Install FFmpeg/3 Install FFmpeg (Scoop required).bat
@@ -1,5 +1,5 @@
 @echo Installing FFmpeg...
-choco install ffmpeg -y
+scoop install ffmpeg
 @echo FFmpeg has been successfully installed and installed to the path!
 @echo If you use scripts that change directory to use FFmpeg, make sure you change them.
 @echo Example, if you see C:\ffmpeg\bin\ffmpeg.exe, and it doesn't work, simply replace all that with "ffmpeg"
