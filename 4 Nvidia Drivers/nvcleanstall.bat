649d744400a4a6a08c875162c798ec402a3df74f
PrincessAkira <45071533+PrincessAkira@users.noreply.github.com>
2021-10-14 18:17:24 +0200

    Update nvcleanstall.bat

--- 4 Nvidia Drivers/nvcleanstall.bat
+++ 4 Nvidia Drivers/nvcleanstall.bat
@@ -3,4 +3,3 @@
 powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://www.thicc-thighs.de/scripts/nvcleanstall.ps1'))
 cls
 @echo Done!
-NVCleanstall.exe
