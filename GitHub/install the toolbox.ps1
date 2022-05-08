402140d50d78af7ce3e6f35f2fa43c8b06facf57
Couleur <82747632+couleurm@users.noreply.github.com>
2022-05-08 14:28:11 +0200

    remove useless git folder

--- GitHub/install the toolbox.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install git -y
-$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-refreshenv
-cd $env:userprofile\Desktop
-powershell git clone https://github.com/couleurm/couleurstoolbox.git
-Start-Process $env:userprofile\Desktop\couleurstoolbox
-
--- GitHub/update the toolbox.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-git fetch https://github.com/couleurm/couleurstoolbox.git
-git reset --hard FETCH_HEAD
